886427ead6fff88c164d09b446c5bbb8b40a8253
Zhumadi1 <dzumaabdykadyrov@gmail.com>
2023-03-31 21:25:56 +0300

    zadacha

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,41 @@
 public class Main {
     public static void main(String[] args) {
-        int[] array = new int[]{16, 44, 200, 55, 3};
-        method(array);
-    }
-    static void method(int[] array) {
+        int[] array = new int[]{1, 44, 20009, 554564989, 33456};
+        method2(array);
+    } static void method2(int[] array) {
         System.out.print("array: ");
         for (int j : array) {
             System.out.print(j + " ");
         }
         System.out.println();
-       int max = array[0];
-       int min = array[0];
-        for (int k : array) {
-            if (k > max) {
-                max = k;
+        int max = array[0];
+        int min = array[0];
+        int indexMax = 0;
+        int indexMin = 0;
+        for (int i = 0; i <array.length ; i++) {
+            if(array[i]>max){
+                max = array[i];
+                indexMax = i;
             }
-        }
-        System.out.println("MAX: "+max);
-        for (int k : array) {
-            if (k < min) {
-                min = k;
+        } System.out.println("MAX: "+max);
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] <= min) {
+                min = array[i];
+                indexMin = i;
                 System.out.println("MIN: " + min);
             }
         }
         System.out.print("array: ");
-            for (int j = 0; j <array.length ; j++) {
-                if(array[j]==max){
-                    array[j] = min;
-                }else if(array[j]==min){
-                    array[j] = max;
-                }
-                System.out.print(array[j]+" ");
+        for (int j = 0; j <array.length ; j++) {
+            if(j==indexMax){
+                array[j] = min;
+            }else if(j==indexMin){
+                array[j] = max;
+            }
+            System.out.print(array[j]+" ");
         }
 
     }
-    
+
 }
